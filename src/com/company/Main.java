46ffa1f2e31d6ab3c92849e50012f61286e2903d
Nikita Smorschok <nsmorschok@gmail.ru>
2022-02-25 15:24:18 +0300

    Homework_two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("нечетные числа от 1 до 99");
-        for (int i = 1; i <= 99;i = i +2){
+        System.out.println("числа от 5 до 1");
+        for (int i = 5; i >= 0; i = i - 1) {
             System.out.println(i);
         }
-        System.out.println("готово");
+        System.out.println("Готово");
     }
-    }
+
+}
 
 
