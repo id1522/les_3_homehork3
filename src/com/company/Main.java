764dcd75c62e33f5bcd8e6794947108b93e270dd
Nikita Smorschok <nsmorschok@gmail.ru>
2022-02-25 21:26:13 +0300

    Homework_two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
 public class Main {
+
     public static void main(String[] args) {
-        for (int i = 0; i > -50; i = i - 5) {
+        int b1 = 2;
+        for ( int i = 10; i <= 20; i = i + 1 ) {
             System.out.println(i);
+            System.out.printf("Числа от 10 до 20 в дводрате %.0f \n", Math.pow(i, b1));
+            // для вывода на экран целого значения используется "%.0f"
+
+        }
         }
     }
-}
 
 
