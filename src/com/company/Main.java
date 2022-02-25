7b698d03e7b5160e2dd3c3e5d0d1843d2007496b
Nikita Smorschok <nsmorschok@gmail.ru>
2022-02-25 15:15:38 +0300

    Homework_two

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("нечетные числа от 1 до 99");
+        for (int i = 1; i <= 99;i = i +2){
+            System.out.println(i);
+        }
+        System.out.println("готово");
+    }
+    }
+
+
